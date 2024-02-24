4236ef7d499eee564230cfbf391e314075ce9402
杨明强 <115909869+YhMjQx@users.noreply.github.com>
2024-02-24 21:02:36 +0800

    Update xml.php

--- xml.php
+++ xml.php
@@ -44,5 +44,37 @@
     }
 
     // 读取XML文件数据保存在PHP的二维数组当中
-    
+        $nodes = $doc->getElementsByTagName("student"); //获取student节点
+    $students = array();  //创建一个空数组用于存储xml文件数据
+    foreach($nodes as $k=>$v) {  // 这里只有 $k 才是 $nodes 的每一个元素，$k 只是一个数字，相当于是student节点个数的下标
+        // echo $k." ";  //0 1 2 3 4 5 代表了6个student节点的下标
+        // echo $v->getElementsByTagName('id')->item(0)->nodeName."=";
+        $students[$k]['id'] = $v->getElementsByTagName("id")->item(0)->nodeValue;  // $v 节点下的第一个id节点的值
+        $students[$k]['name'] = $v->getElementsByTagName("name")->item(0)->nodeValue;  // $v 节点下的第一个id节点的值
+        $students[$k]['sex'] = $v->getElementsByTagName("sex")->item(0)->nodeValue;  // $v 节点下的第一个id节点的值
+        $students[$k]['age'] = $v->getElementsByTagName("age")->item(0)->nodeValue;  // $v 节点下的第一个id节点的值
+        $students[$k]['degree'] = $v->getElementsByTagName("degree")->item(0)->nodeValue;  // $v 节点下的第一个id节点的值
+        $students[$k]['school'] = $v->getElementsByTagName("school")->item(0)->nodeValue;  // $v 节点下的第一个id节点的值
+        // print_r($student[$k]['id']);
+        // echo "<br>";
+    }
+    // echo "<br>";
+    foreach($students as $student) {
+        print_r($student);
+        echo "<br>";
+    }
+    echo "</p>";
+
+    /**以上遍历代码可以优化为 */
+    $tags = array('id','name','sex','age','degree','school');
+    foreach($nodes as $k=>$v) {
+        foreach($tags as $tag) {
+            $students[$k][$tag] = $v->getElementsByTagName($tag)->item(0)->nodeValue;
+        }
+    }
+    foreach($students as $student) {
+        print_r($student);
+        echo "<br>";
+    }
+    echo "</p>";
 ?>
