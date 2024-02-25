674443bf541b03da75b6f151bd5cbd9ac65ef181
杨明强 <115909869+YhMjQx@users.noreply.github.com>
2024-02-25 23:29:12 +0800

    Update xml.php

--- xml.php
+++ xml.php
@@ -78,3 +78,31 @@
     }
     echo "</p>";
 ?>
+
+    /**读取 firewall.xml的所有端口 */
+    $doc1->load('../firewalld.xml');
+    $nodes = $doc1->getElementsByTagName('port');
+    foreach($nodes as $node) {
+        $attr = $node->attributes;
+        echo $attr->item(1)->nodeName."=";
+        echo $attr->item(1)->nodeValue."  ";
+    }
+    echo "<br>";
+
+    /**使用PHP修改xml文件节点的内容 */
+    $doc2 = new DOMDocument();
+    $doc2->preserveWhiteSpace = false;  // 该值默认时 true ，意思是默认保留空白，但如果单单设置参数为false，就会将节点前的所有空白删除，此时所有文件内容全都在一行，很丑陋，于是有了下面的参数
+    $doc2->formatOutput = true;  // 添加这个参数，使得可以格式化输出文本内容，确保保持xml文件格式
+    $doc2->load('../student.xml');
+    // $doc2->load('../firewalld.xml');
+    $nodes = $doc2->getElementsByTagName('student');
+    $nodes->item(0)->attributes->item(0)->nodeValue = '11';
+    $doc2->save('../student.xml');
+
+    $nodes->item(0)->childNodes->item(1)->nodeValue = 'ymqyyds';
+    $doc2->save('../student.xml');
+    // 以上内容修改失败，原因是因为xml文件格式化时前面都有空白，默认时，该空白也会被认为是一个节点，我们可以使用下面foreach循环进行测试，浏览器默认将多个空格转换为一个空格输出
+    // foreach($nodes->item(0)->childNodes as $node) {
+    //     echo $node->nodeValue."<br>";
+    //     // 输出结果和正常的很不一样，为了解决这个问题我们需要加入两个参数  $doc2->preserveWhiteSpace = false  $doc2->formatOutput = true 然后这段代码运行才能成功
+    // }
