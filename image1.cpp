408ec532d029a54c5cdb75b2d465f76e1c086134
杨明强 <115909869+YhMjQx@users.noreply.github.com>
2024-02-29 21:23:21 +0800

    Add files via upload

---
+++ image1.cpp
@@ -0,0 +1,28 @@
+//#include<iostream>
+//using namespace std;
+//int main(){
+//	int x = 1;
+//	int y = 2;
+//	int n = 64;
+//	cout << "请输入x的值: ";
+//	cin >> x;
+//	cout << "请输入x的值: ";
+//	cin >> y;
+//
+//	int image[64][64];
+//	for (int a = 0; a < 64; ++a){
+//		for (int b = 0; b < 64; ++b){
+//			if (a == b){
+//				image[a][b] = y;
+//			}
+//			else {
+//				image[a][b] = x;
+//			}
+//			//cout << image[a][b] << " ";
+//			cout << image[a][b];
+//		}
+//		cout << endl;
+//
+//	}
+//	return 0;
+//}
